f0dc98894c58db46629590f7e6aaef4ccd1ac244
alejandra <aleee5858@gmail.com>
2024-03-30 04:17:02 -0300

    (aun me queda)

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,27 +1,32 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./NavBar.module.css"
+import logo from "../../assets/icon.png"
 
 const NavBar = () => {
     return (
-    <div>
-        <ul>
+        <ul className={styles.container}>
             <li>
-                <Link>En cartelera</Link>
+                <img src={logo} className="logo" alt="" />
+            </li>
+            
+
+            <li>
+                <Link className={styles.link} to={"now_playing"}>En cartelera</Link>
             </li>
 
             <li>
-                <Link>Populares</Link>
+                <Link className={styles.link} to={"popular"}>Populares</Link>
             </li>
 
             <li>
-                <Link>Mejor Calificados</Link>
+                <Link className={styles.link} to={"top_related"}>Mejor Calificados</Link>
             </li>
 
             <li>
-                <Link>Proximamente</Link>
+                <Link className={styles.link} to={"upcoming"}>Proximamente</Link>
             </li>
         </ul>
-    </div>
     )
 }
 
